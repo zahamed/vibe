057347f2a3e7f9838112b546770a3929902e5a46
zahamed <zahamed@gmail.com>
2025-09-11 23:35:30 +0600

    03:trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,72 @@
 
+//Trpc Prefeactching in cleint Component
 
+import { getQueryClient, trpc } from '@/trpc/server';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 
-const Page =  () => {
+import { Client } from "./client";
+import { Suspense } from 'react';
+
+const Page = async () => {
+
+  console.log("Celient Prefeactching COMPONENT");
+
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({ text: "Zahir Prefetch" }));
 
   return (
-    <div>
-      Hellow
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
   );
 }
 
 export default Page;
+
+//Server Component
+
+/**import { caller } from '@/trpc/server';
+
+const Page = async () => {
+
+console.log("SERVER COMPONENT");
+
+const data = await caller.createAI({text: "from server component"});
+
+  return (
+    <div>
+      {JSON.stringify(data)}
+    </div>
+  );
+}
+
+export default Page;**/
+
+
+//Client Component
+
+
+/**"use client";
+
+import { useQuery } from "@tanstack/react-query";
+
+import { useTRPC } from "@/trpc/client";
+
+const Page =  () => {
+
+console.log("Client COMPONENT");
+
+  const trpc= useTRPC();
+  const { data } = useQuery(trpc.createAI.queryOptions({ text: "from Zahir" }));
+
+  return (
+    <div>
+      {JSON.stringify(data)}
+    </div>
+  );
+}
+
+export default Page;**/
