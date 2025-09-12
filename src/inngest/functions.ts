619daaec458458662b6b62ffd2bef23797b49089
zahamed <zahamed@gmail.com>
2025-09-12 23:24:03 +0600

    05: ai jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,17 +1,45 @@
+
+import { openai, anthropic, gemini, createAgent } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
+import { success } from "zod";
+
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-   
-    await step.sleep("wait-a-moment-download", "30s");
+  async ({ event }) => {
 
-    await step.sleep("wait-a-moment-transscript", "10s");
+    // Create a new agent with a system prompt (you can add optional tools, too)
+    const summerizer = createAgent({
+      name: "summerizer",
+      system: "You are an expert summerizer.  You summerize in 2 words.",
+      //model: openai({ model: "gpt-4o" }),
+      //model: anthropic({ model: "claude-3-5-haiku-latest" }),
+      //model: gemini({ model: "gemini-1.5-flash" }),
+      model: gemini({ model: "gemini-2.0-flash" }),
+    });
 
-    await step.sleep("wait-a-moment-summary", "5s");
+    //const { output } = await summerizer.run(`Summarize the following text: ${event.data.value}`);
 
-    return { message: `Hello ${event.data.email}!` };
-    
+    //console.log("Summerizer:", output);
+
+     // Create a new agent with a system prompt (you can add optional tools, too)
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert next.js developer.  You write readable, maintainable code. You write simple Next.js & React Snippets.",
+      //model: openai({ model: "gpt-4o" }),
+      //model: anthropic({ model: "claude-3-5-haiku-latest" }),
+      //model: gemini({ model: "gemini-1.5-flash" }),
+      model: gemini({ model: "gemini-2.0-flash" }),
+    });
+
+
+    const { output } = await codeAgent.run(`Write the following snippet: ${event.data.value}`);
+
+    console.log("codesnippet:", output);
+
+    return { output };
+
   },
 );
